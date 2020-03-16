5ed24913596235da8cf5b1d6c3df267b3be658e5
Nils Oliver Kroeger (NIKR) <NilsOliver.Kroeger@softwareag.com>
2020-03-16 16:15:34 +0100

    Added additional metrics for messaging triggers and connections (jms and native)

---
+++ source/IS/packages/WxPlatformInsight/code/source/platforminsight/monitoring/integrationserver/impl/service/serverAPI.java
@@ -0,0 +1,26 @@
+package platforminsight.monitoring.integrationserver.impl.service;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class serverAPI
+
+{
+	// ---( internal utility methods )---
+
+	final static serverAPI _instance = new serverAPI();
+
+	static serverAPI _newInstance() { return new serverAPI(); }
+
+	static serverAPI _cast(Object o) { return (serverAPI)o; }
+
+	// ---( server methods )---
+
+}
+
--- source/IS/packages/WxPlatformInsight/ns/wx/platforminsight/monitoring/pipelineUtils/impl/service/getMetricFromPipeline/java.frag
+++ source/IS/packages/WxPlatformInsight/ns/wx/platforminsight/monitoring/pipelineUtils/impl/service/getMetricFromPipeline/java.frag
@@ -3,6 +3,6 @@
 <Values version="2.0">
   <value name="name">getMetricFromPipeline</value>
   <value name="encodeutf8">true</value>
-  <value name="body">SURhdGFNYXAgcGxNYXAgPSBuZXcgSURhdGFNYXAocGlwZWxpbmUpOw0KcGxNYXAucHV0KCJtZXRy
-aWMiLCBwbE1hcC5nZXQocGxNYXAuZ2V0KCJtZXRyaWNOYW1lIikpKTs=</value>
+  <value name="body">SURhdGFNYXAgcGxNYXAgPSBuZXcgSURhdGFNYXAocGlwZWxpbmUpOwpwbE1hcC5wdXQoIm1ldHJp
+YyIsIHBsTWFwLmdldChwbE1hcC5nZXQoIm1ldHJpY05hbWUiKSkpOw==</value>
 </Values>
