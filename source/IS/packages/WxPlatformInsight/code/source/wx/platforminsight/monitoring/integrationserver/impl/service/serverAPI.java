e4a8dc235dcaa470acbdd746009fe1d1d13cc6d2
X60000279 <X60000279@NB00001856.BG10.BGFE.Local>
2023-12-01 14:29:07 +0100

    label all UM triggers with the package name

--- source/IS/packages/WxPlatformInsight/code/source/wx/platforminsight/monitoring/integrationserver/impl/service/serverAPI.java
+++ source/IS/packages/WxPlatformInsight/code/source/wx/platforminsight/monitoring/integrationserver/impl/service/serverAPI.java
@@ -7,6 +7,9 @@ import com.wm.util.Values;
 import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
 // --- <<IS-START-IMPORTS>> ---
+import com.wm.app.b2b.server.dispatcher.trigger.Trigger;
+import com.wm.lang.ns.NSService;
+import com.wm.app.b2b.server.ns.Namespace;
 import com.softwareag.util.IDataMap;
 import com.wm.app.b2b.server.ServerAPI;
 // --- <<IS-END-IMPORTS>> ---
@@ -27,6 +30,43 @@ public final class serverAPI
 
 
 
+	public static final void getPackageNameOfTrigger (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(getPackageNameOfTrigger)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required nsName
+		// [i] field:0:optional default
+		// [o] field:0:required packageName
+		IDataCursor pipelineCursor = pipeline.getCursor();
+		String	nsName    = IDataUtil.getString( pipelineCursor, "nsName" );
+		String  defaultPN = IDataUtil.getString( pipelineCursor, "default" );
+		pipelineCursor.destroy();
+		
+		if ( defaultPN == null ) 
+			defaultPN = "unknown";
+		String       back = defaultPN;
+		
+		if ( nsName != null && nsName.length() > 0 ) {
+			try {
+			Trigger    service = (Trigger) Namespace.current().getNode( nsName );
+			back               = service.getPackage().getName();
+			}
+			catch (Throwable e) {
+				//Suppress exception
+			}
+		}
+		pipelineCursor = pipeline.getCursor();
+		IDataUtil.put(pipelineCursor, "packageName", back );
+		pipelineCursor.destroy();
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
 	public static final void getServerName (IData pipeline)
         throws ServiceException
 	{
@@ -35,6 +75,7 @@ public final class serverAPI
 		// [o] field:0:required serverName
 		String serverName = ServerAPI.getServerName();
 		new IDataMap(pipeline).put("serverName", serverName);
+			
 		// --- <<IS-END>> ---
 
                 
